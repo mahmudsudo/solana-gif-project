08097878a41ac0c21dc3dc5fd8f04e23b9c85ab9
mahmud bello <blinkztyler@gmail.com>
2022-09-22 00:00:18 +0100

    a solana gif website with the anchor project

--- tests/mygifproject.js
+++ tests/mygifproject.js
@@ -1,13 +1,58 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
 
-describe("mygifproject", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Mygifproject;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Mygifproject;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+  // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 GIF Count', account.totalGifs.toString())
+       // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
+        await program.rpc.addGif("https://media1.giphy.com/media/8qUjDf9PZlHZ6/giphy.webp?cid=ecf05e47zcg86785f5bcrgfn57ep9aykhmtn030o2bbc1hgz&rid=giphy.webp&ct=g", {
+          accounts: {
+            baseAccount: baseAccount.publicKey,
+            user: provider.wallet.publicKey,
+          },
+        });
+
+        // Call the account.
+        account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+        console.log('👀 GIF Count', account.totalGifs.toString())
+
+        // Access gif_list on the account!
+        console.log('👀 GIF List', account.gifList)
+      }
+
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+
+  runMain();
